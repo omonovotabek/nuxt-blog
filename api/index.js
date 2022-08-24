e555c66f52afed0d1da23818aeb8c80e474d1ebe
omonovotabek <omonovotabek9541537@gmail.com>
2022-08-24 12:53:55 +0500

    add

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());  
 app.use(fileUpload({}))   
-app.use(express.static('static'))   
+// app.use(express.static('static'))   
 app.use(routes);
 app.use(errorHandler)
 // app.use(morgan("dev"))
