b92e5e08c4a76df9f5587003bf3b8f3a0023367c
omonovotabek <omonovotabek9541537@gmail.com>
2022-08-24 12:10:01 +0500

    asss

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload")
 const cors = require("cors");
-// const path = require("path");
+const path = require("path");
 const errorHandler = require("./middleware/error")
 const routes = require("./routes")
 // const morgan = require('morgan')
@@ -18,6 +18,12 @@ app.use(routes);
 app.use(errorHandler)
 // app.use(morgan("dev"))
 
+if (process.env.NODE_ENV) {
+  app.use(express.static(path.resolve(process.cwd(), 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(process.cwd(), 'client/build/index.html'))
+  })
+}
 
 mongoose
   .connect(`${process.env.DB_URL}`)
